af9f5d8d577023f9726eb7dafbdd0f758dcbaddb
Alex Bakus <alex@finask.ca>
2013-08-20 21:09:50 -0400

    Initial Commit

---
+++ StopWatch/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  StopWatch
+#
+#  Created by Alexander Bakus on 2013-08-20.
+#  Copyright 2013 Alexander Bakus. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
