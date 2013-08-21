dcccdf94ef6cdc2a85e206e69ebbd4b278fd4b9b
Alex Bakus <alex@finask.ca>
2013-08-20 22:35:13 -0400

    - adding text field, 2 push buttons, and ns timer functionality

--- StopWatch/AppDelegate.rb
+++ StopWatch/AppDelegate.rb
@@ -8,8 +8,48 @@
 
 class AppDelegate
     attr_accessor :window
+    attr_accessor :textField # This creates an outlet named textField
+    attr_accessor :pushButtonStart # This creates an outlet named pushButtonStart
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+    end
+    
+    def startTimer(sender)
+        # This method is called when the start button is clicked.
+        if textField.stringValue.nil? && pushButtonStart.title == "start"
+            @time = 0.0
+            timerStart(@time)
+            pushButtonStart.title = "pause"
+            elsif pushButtonStart.title == "pause"
+            stopTimer(true)
+            pushButtonStart.title = "start"
+            else
+            @time = textField.stringValue.to_f
+            timerStart(@time)
+            pushButtonStart.title = "pause"
+        end
+    end
+    
+    def stopTimer(sender)
+        # This method is called when the stop button is clicked.
+        if @timer
+            @timer.invalidate
+            @timer = nil
+        end
+    end
+    
+    def timerStart(time)
+        if @timer.nil?
+            @time = time
+            @timer = NSTimer
+            .scheduledTimerWithTimeInterval(0.1, target: self, selector: "timerHandler:", userInfo: nil, repeats: true)
+        end
+    end
+    
+    def timerHandler(userInfo)
+        @time += 0.1
+        string = sprintf("%.1f", @time)
+        textField.setStringValue(string)
     end
 end
 
